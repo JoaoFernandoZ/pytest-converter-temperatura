09f1afe339e70f0b3b246c51e7183078e01e90c1
joaofernandoz <joaofernandooliveirazacarias@gmail.com>
2025-09-16 08:16:06 -0300

    Add: "Error Handling" de validação de valores e tipos | Fix: Ajustes necessários na função

--- conversores_temp.py
+++ conversores_temp.py
@@ -1,17 +1,18 @@
 def converter_temperatura(de_temperatura: str, para_temperatura: str, valor: int|float):
     unidades = ("KELVIN","CELSIUS","FAHRENHEIT")
-    de_temperatura = de_temperatura.upper()
-    para_temperatura = para_temperatura.upper()
 
     if not all(isinstance(unidade, str) for unidade in (de_temperatura, para_temperatura)):
         raise TypeError("As unidades devem ser textos em string")
-    elif not (de_temperatura in unidades and para_temperatura in unidades):
+    elif not (de_temperatura.upper() in unidades and para_temperatura.upper() in unidades):
         raise ValueError("Escala de temperatura inválida")
     elif not isinstance(valor, (int, float)):
         raise TypeError("O valor passado deve ser numérico")
     elif de_temperatura.upper() == "KELVIN" and valor < 0:
         raise ValueError("Temperatura em KELVIN não pode ser negativa")
     
+    de_temperatura = de_temperatura.upper()
+    para_temperatura = para_temperatura.upper()
+
     if de_temperatura == "CELSIUS":
         match para_temperatura:
             case "KELVIN":
--- test_conversores_temp.py
+++ test_conversores_temp.py
@@ -21,7 +21,7 @@ from conversores_temp import converter_temperatura
         # Caso 5: Fahrenheit para Celsius
         ("FAHRENHEIT", "CELSIUS", 50,   10),
         # Caso 6: Fahrenheit para Kelvin
-        ("FAHRENHEIT", "KELVIN", 50,   283.15),
+        ("FAHRENHEIT", "KELVIN", 50,    283.15),
     ],
     ids = ["kelvin_celsius", "kelvin_fahrenheit", "celsius_kelvin", "celsius_fahrenheit", "fahrenheit_celsius", "fahrenheit_kelvin"]
 )
@@ -29,3 +29,43 @@ def test_converter_temperatura_validos(de_temperatura, para_temperatura, valor, 
     resultado = converter_temperatura(de_temperatura, para_temperatura, valor)
 
     assert resultado == pytest.approx(resultado_esperado)
+
+
+# --- [ Testes de Tratamento de Erros (Error Handling) ] --- #
+
+@pytest.mark.error_handling
+@pytest.mark.parametrize(
+        "de_temperatura, para_temperatura, valor, erro_esperado",
+    [
+        # Caso 1: Unidade inválida no primeiro paramêtro
+        ("ALEX", "CELSIUS", 50,         "Escala de temperatura inválida"),
+        # Caso 2: Unidade inválida no segundo paramêtro
+        ("KELVIN", "IVO", 50,           "Escala de temperatura inválida"),
+
+        # Caso 3: Temperatura Kelvin negativa
+        ("KELVIN", "FAHRENHEIT", -1,    "Temperatura em KELVIN não pode ser negativa"),
+    ],
+    ids = ["unidade1_invalida", "unidade2_invalida", "kelvin_negativo"]
+)
+def test_converter_temperatura_valores_invalidos(de_temperatura, para_temperatura, valor, erro_esperado):
+    with pytest.raises(ValueError, match=erro_esperado):
+        converter_temperatura(de_temperatura, para_temperatura, valor)
+
+
+@pytest.mark.error_handling
+@pytest.mark.parametrize(
+        "de_temperatura, para_temperatura, valor, erro_esperado",
+    [
+        # Caso 1: Unidade do tipo 'lista' no primeiro paramêtro
+        (["KELVIN"], "CELSIUS", 50,         "As unidades devem ser textos em string"),
+        # Caso 2: Unidade do tipo 'int' no segundo paramêtro
+        ("FAHRENHEIT", 1298, 50,            "As unidades devem ser textos em string"),  
+
+        # Caso 3: Temperatura do tipo 'string'
+        ("FAHRENHEIT", "KELVIN", "50",      "O valor passado deve ser numérico"),
+    ],
+    ids = ["unidade1_lista", "unidade2_int", "temperatura_string"]
+)
+def test_converter_temperatura_tipos_invalidos(de_temperatura, para_temperatura, valor, erro_esperado):
+    with pytest.raises(TypeError, match=erro_esperado):
+        converter_temperatura(de_temperatura, para_temperatura, valor)
